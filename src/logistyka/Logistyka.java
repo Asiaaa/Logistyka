285b61b2e06288c3faf7f86641d77e59dfe0b095
Asiaaa <joanna.wolna@ymail.com>
2015-05-29 12:35:08 +0200

    Connect

--- src/logistyka/Logistyka.java
+++ src/logistyka/Logistyka.java
@@ -18,6 +18,7 @@ public class Logistyka {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("First upload.");
+        System.out.println("Paczki");
     }
     
 }
